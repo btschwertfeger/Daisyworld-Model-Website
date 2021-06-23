88e3583e53ff181ecf0bc3f49bfda189448ae89b
Benjamin Thomas Schwertfeger <benjamin@b-schwertfeger.de>
2021-06-23 16:31:46 +0200

    new functions; bug fixes; new graphs;

--- layout_scripts.js
+++ layout_scripts.js
@@ -8,7 +8,7 @@
 
 /* GENERAL VARIABLES*/
 let dw_sliders = document.getElementsByName("dw_slider");
-let dw_variabels = ['alb_white', 'alb_black', 'alb_barren', 'insul', 'drate', 'Sflux_min', 'Sflux_max', 'Sflux_step'];
+let dw_variabels = ['alb_white', 'alb_black', 'alb_barren', 'insul', 'drate', 'optTw', 'optTb', 'Sflux_min', 'Sflux_max', 'Sflux_step'];
 
 
 /* RESET BUTTON  */
@@ -20,12 +20,14 @@ resetBtn.onclick = function () {
         document.getElementById(element.id).value = default_value;
         document.getElementById(dw_variabels[index] + '_sliderAmount').innerHTML = default_value;
     });
-    document.getElementById('reverse').checked = false;
     document.getElementById('KELVIN_OFFSET').value = 273.15;
 }
 
 /* SLIDER  */
 for (let entry = 0; entry < dw_sliders.length; entry++) {
+    dw_sliders[entry].oninput = function () {
+        document.getElementById(dw_sliders[entry].id + 'rAmount').innerHTML = document.getElementById(dw_sliders[entry].id).value;
+    }
     dw_sliders[entry].onchange = function () {
         window.updatePlots({
             alb_white: document.getElementById('alb_white_slide').value,
@@ -33,17 +35,22 @@ for (let entry = 0; entry < dw_sliders.length; entry++) {
             alb_barren: document.getElementById('alb_barren_slide').value,
             insul: document.getElementById('insul_slide').value,
             drate: document.getElementById('drate_slide').value,
+            optTw: document.getElementById('optTw_slide').value,
+            optTb: document.getElementById('optTb_slide').value,
             Sflux_min: document.getElementById('Sflux_min_slide').value,
             Sflux_max: document.getElementById('Sflux_max_slide').value,
             Sflux_step: document.getElementById('Sflux_step_slide').value,
             KELVIN_OFFSET: document.getElementById('KELVIN_OFFSET').value,
-            REVERSE: (document.getElementById('reverse').checked) ? true : false,
         })
-        document.getElementById(dw_sliders[entry].id + 'rAmount').innerHTML = document.getElementById(dw_sliders[entry].id).value;
     }
 }
 /* REVERSE AND KELVIN INPUT */
-let SOMEGOODVARIABLENAME = ['reverse', 'KELVIN_OFFSET', 'showTempWithoutLife-checkbox'];
+let SOMEGOODVARIABLENAME = ['showWhiteDinc-checkbox', 'showBlackDinc-checkbox',
+    'showWhiteDdec-checkbox', 'showBlackDdec-checkbox',
+    'showTotalAmountD4incL-checkbox', 'showTotalAmountD4decL-checkbox',
+    'showglobTinc-checkbox', 'showglobTdec-checkbox',
+    'showTempWithoutLife-checkbox', 'KELVIN_OFFSET'
+];
 SOMEGOODVARIABLENAME.forEach(function (entry, index) {
     document.getElementById(entry).onchange = function () {
         window.updatePlots({
@@ -52,11 +59,12 @@ SOMEGOODVARIABLENAME.forEach(function (entry, index) {
             alb_barren: document.getElementById('alb_barren_slide').value,
             insul: document.getElementById('insul_slide').value,
             drate: document.getElementById('drate_slide').value,
+            optTw: document.getElementById('optTw_slide').value,
+            optTb: document.getElementById('optTb_slide').value,
             Sflux_min: document.getElementById('Sflux_min_slide').value,
             Sflux_max: document.getElementById('Sflux_max_slide').value,
             Sflux_step: document.getElementById('Sflux_step_slide').value,
             KELVIN_OFFSET: document.getElementById('KELVIN_OFFSET').value,
-            REVERSE: (document.getElementById('reverse').checked) ? true : false,
         });
     }
 });
--- main.js
+++ main.js
@@ -65,14 +65,17 @@ function range(start, end, step) {
 
 /* DAISYWORLD */
 let KELVIN_OFFSET = 273.15;
+let g_optTw = 22.5,
+    g_optTb = 22.5;
 window.default_parameters = {
-    REVERSE: false,
     // Temperatures
     KELVIN_OFFSET: KELVIN_OFFSET,
+    optTb: g_optTb,
+    optTw: g_optTw,
     Td_min: 5 + KELVIN_OFFSET,
     Td_max: 40 + KELVIN_OFFSET,
-    Td_ideal_black: 22.5 + KELVIN_OFFSET,
-    Td_ideal_white: 22.5 + KELVIN_OFFSET,
+    Td_ideal_black: g_optTb + KELVIN_OFFSET,
+    Td_ideal_white: g_optTw + KELVIN_OFFSET,
 
     // Albedo
     alb_white: 0.75,
@@ -107,12 +110,77 @@ window.updatePlots = function (input = window.default_parameters) {
     document.getElementById('daisyworld-area-plot').remove();
     document.getElementById('daisyworld-area-plot-container').innerHTML = '<canvas id="daisyworld-area-plot"></canvas>';
 
-    let black_data = new Array(RESULT['area_black_vec'].length),
-        white_data = new Array(RESULT['area_white_vec'].length);
+    let black_data_inc = new Array(RESULT['area_black_vec_inc'].length),
+        white_data_inc = new Array(RESULT['area_white_vec_inc'].length),
+        black_data_dec = new Array(RESULT['area_black_vec_dec'].length),
+        white_data_dec = new Array(RESULT['area_white_vec_dec'].length);
 
-    for (let entry = 0; entry < black_data.length; entry++) {
-        black_data[entry] = RESULT['area_black_vec'][entry] * 100;
-        white_data[entry] = RESULT['area_white_vec'][entry] * 100;
+    for (let entry = 0; entry < black_data_inc.length; entry++) {
+        black_data_inc[entry] = RESULT['area_black_vec_inc'][entry] * 100;
+        white_data_inc[entry] = RESULT['area_white_vec_inc'][entry] * 100;
+        black_data_dec[entry] = RESULT['area_black_vec_dec'][entry] * 100;
+        white_data_dec[entry] = RESULT['area_white_vec_dec'][entry] * 100;
+    }
+
+    let areaData = [];
+    if (document.getElementById('showWhiteDinc-checkbox').checked) {
+        areaData.push({
+            label: 'white increasing L',
+            data: white_data_inc,
+            fill: false,
+            borderColor: 'rgb(255,0,03)',
+            pointRadius: 0
+        });
+    }
+    if (document.getElementById('showWhiteDdec-checkbox').checked) {
+        areaData.push({
+            label: 'white decreasing L',
+            data: white_data_dec,
+            fill: false,
+            borderColor: 'rgb(180,100,20)',
+            pointRadius: 0
+        });
+    }
+
+    if (document.getElementById('showBlackDinc-checkbox').checked) {
+        areaData.push({
+            label: 'black increasing L',
+            data: black_data_inc,
+            fill: false,
+            borderColor: 'rgb(0,0,0)',
+            pointRadius: 0
+        });
+    }
+
+    if (document.getElementById('showBlackDdec-checkbox').checked) {
+        areaData.push({
+            label: 'black decreasing L',
+            data: black_data_dec,
+            fill: false,
+            borderColor: 'rgb(150,150,150)',
+            pointRadius: 0
+        });
+    }
+
+    if (document.getElementById('showTotalAmountD4incL-checkbox').checked) {
+        // console.log(RESULT['total_amount_daisy4incL'])
+        areaData.push({
+            label: 'total amount of daisies for incr. L',
+            data: RESULT['total_amount_daisy4incL'],
+            fill: false,
+            borderColor: 'rgb(127,255,0)',
+            pointRadius: 0,
+        })
+    }
+
+    if (document.getElementById('showTotalAmountD4decL-checkbox').checked) {
+        areaData.push({
+            label: 'total amount of daisies for incr. L',
+            data: RESULT['total_amount_daisy4decL'],
+            fill: false,
+            borderColor: 'rgb(0,0,255)',
+            pointRadius: 0,
+        })
     }
 
     let ctx1 = document.getElementById('daisyworld-area-plot');
@@ -120,19 +188,7 @@ window.updatePlots = function (input = window.default_parameters) {
         type: 'line',
         data: {
             labels: RESULT['fluxes'],
-            datasets: [{
-                label: 'black',
-                data: black_data,
-                fill: false,
-                borderColor: 'rgb(0,0,0)',
-                pointRadius: 0
-            }, {
-                label: 'white',
-                data: white_data,
-                fill: false,
-                borderColor: 'rgb(255,0,03)',
-                pointRadius: 0
-            }],
+            datasets: areaData,
         },
         options: {
             responsive: true,
@@ -200,30 +256,45 @@ window.updatePlots = function (input = window.default_parameters) {
     document.getElementById('daisyworld-temperature-plot').remove();
     document.getElementById('daisyworld-temperature-plot-container').innerHTML = '<canvas id="daisyworld-temperature-plot"></canvas>';
 
-    let temp_lumi_data = new Array(RESULT['Tp_vec'].length);
+    let temp_lumi_data_inc = new Array(RESULT['Tp_vec_inc'].length),
+        temp_lumi_data_dec = new Array(RESULT['Tp_vec_dec'].length);
 
-    for (let entry = 0; entry < temp_lumi_data.length; entry++) {
-        temp_lumi_data[entry] = RESULT['Tp_vec'][entry] - RESULT['KELVIN_OFFSET'];
+    for (let entry = 0; entry < temp_lumi_data_inc.length; entry++) {
+        temp_lumi_data_inc[entry] = RESULT['Tp_vec_inc'][entry] - RESULT['KELVIN_OFFSET'];
+        temp_lumi_data_dec[entry] = RESULT['Tp_vec_dec'][entry] - RESULT['KELVIN_OFFSET'];
     }
 
     let temp_data = [{
-        label: 'global temperature',
-        data: temp_lumi_data,
-        fill: false,
-        borderColor: 'rgb(255,0,0)',
-        pointRadius: 0,
-    }, {
         label: 'optimal temperature',
-        data: new Array(temp_lumi_data.length).fill(22.5),
+        data: new Array(temp_lumi_data_inc.length).fill(22.5),
         fill: false,
         borderColor: 'rgb(236,193,20)',
         pointRadius: 0,
     }];
 
+    if (document.getElementById('showglobTinc-checkbox').checked) {
+        temp_data.push({
+            label: 'global temperature incr. L',
+            data: temp_lumi_data_inc,
+            fill: false,
+            borderColor: 'rgb(255,0,0)',
+            pointRadius: 0,
+        });
+    }
+    if (document.getElementById('showglobTdec-checkbox').checked) {
+        temp_data.push({
+            label: 'global temperature decr. L',
+            data: temp_lumi_data_dec,
+            fill: false,
+            borderColor: 'rgb(0,0,255)',
+            pointRadius: 0,
+        });
+    }
+
     if (document.getElementById('showTempWithoutLife-checkbox').checked) {
         temp_data.push({
             label: 'Temperature without life',
-            data: RESULT['temp_without_life'],
+            data: RESULT['temp_without_life_inc'],
             fill: false,
             borderColor: 'rgb(0,0,0)',
             pointRadius: 0,
@@ -299,17 +370,15 @@ window.updatePlots = function (input = window.default_parameters) {
     });
 }
 
-// CALCULATION
-function doDaisyWorld(input = window.default_parameters) {
-
-    let REVERSE = input['REVERSE'];
-
+function computeDaisyworld(input, kind) {
     // Temperatures
     let KELVIN_OFFSET = parseFloat(input['KELVIN_OFFSET']);
+    let optTw = parseFloat(input['optTw']),
+        optTb = parseFloat(input['optTb']);
     let Td_min = 5 + KELVIN_OFFSET,
         Td_max = 40 + KELVIN_OFFSET;
-    let Td_ideal_black = 22.5 + KELVIN_OFFSET,
-        Td_ideal_white = 22.5 + KELVIN_OFFSET;
+    let Td_ideal_black = optTb + KELVIN_OFFSET,
+        Td_ideal_white = optTw + KELVIN_OFFSET;
     let Tp = 0;
 
     // Albedo
@@ -337,7 +406,8 @@ function doDaisyWorld(input = window.default_parameters) {
 
     // Init
     let fluxes = range(Sflux_min, Sflux_max, Sflux_step);
-    if (REVERSE) {
+
+    if (kind == "decr") {
         fluxes = fluxes.reverse();
     }
 
@@ -414,15 +484,54 @@ function doDaisyWorld(input = window.default_parameters) {
 
     });
 
-    const RESULT = {
-        fluxes: fluxes,
-        area_black_vec: area_black_vec,
-        area_white_vec: area_white_vec,
-        area_barren_vec: area_barren_vec,
-        temp_without_life: temp_without_life_vec,
-        Tp_vec: Tp_vec,
-        KELVIN_OFFSET: KELVIN_OFFSET,
+    let total_amount_daisies = new Array(area_black_vec.length);
+    for (let entry = 0; entry < total_amount_daisies.length; entry++) {
+        total_amount_daisies[entry] = (area_black_vec[entry] + area_white_vec[entry]) * 100;
     }
+    if (kind == "incr") {
+        return {
+            fluxes_inc: fluxes,
+            area_black_vec_inc: area_black_vec,
+            area_white_vec_inc: area_white_vec,
+            area_barren_vec_inc: area_barren_vec,
+            temp_without_life_inc: temp_without_life_vec,
+            total_amount_daisy4incL: total_amount_daisies,
+            Tp_vec_inc: Tp_vec,
+        };
+    } else {
+        return {
+            area_black_vec_dec: area_black_vec.reverse(),
+            area_white_vec_dec: area_white_vec.reverse(),
+            area_barren_vec_dec: area_barren_vec.reverse(),
+            temp_without_life_dec: temp_without_life_vec.reverse(),
+            total_amount_daisy4decL: total_amount_daisies.reverse(),
+            Tp_vec_dec: Tp_vec.reverse(),
+        };
+
+    }
+}
+// CALCULATION
+function doDaisyWorld(input = window.default_parameters) {
+    let res4incrL = computeDaisyworld(input, "incr");
+    let res4decrL = computeDaisyworld(input, "decr");
+
+    const RESULT = {
+        fluxes: res4incrL['fluxes_inc'],
+        area_black_vec_inc: res4incrL['area_black_vec_inc'],
+        area_black_vec_dec: res4decrL['area_black_vec_dec'],
+        area_white_vec_inc: res4incrL['area_white_vec_inc'],
+        area_white_vec_dec: res4decrL['area_white_vec_dec'],
+        area_barren_vec_inc: res4incrL['area_barren_vec_inc'],
+        area_barren_vec_dec: res4decrL['area_barren_vec_dec'],
+        temp_without_life_inc: res4incrL['temp_without_life_inc'],
+        temp_without_life_dec: res4incrL['temp_without_life_dec'],
+        total_amount_daisy4incL: res4incrL['total_amount_daisy4incL'],
+        total_amount_daisy4decL: res4decrL['total_amount_daisy4decL'],
+        Tp_vec_inc: res4incrL['Tp_vec_inc'],
+        Tp_vec_dec: res4decrL['Tp_vec_dec'],
+        KELVIN_OFFSET: input['KELVIN_OFFSET'],
+    }
+
     if (input == window.default_parameters) {
         window.defaultResult = RESULT;
     }
